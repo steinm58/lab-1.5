d0927ca1cf403a4c104de3ac6383b6da9dd2f388
steinm58 <steinm58@msu.edu>
2024-01-22 22:26:54 -0500

    trying to fix navigation

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,4 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addPassthroughCopy("css");
-    eleventyConfig.addPlugin(eleventyNavigationPlugin);
 }
